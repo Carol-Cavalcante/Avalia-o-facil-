e62c49558d0738aa3ee4f4fb9b1df044596547e6
Carol <carolcavalcante.002@gmail.com>
2019-06-10 17:52:07 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carol_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta;
+
+
+            do
+            {
+                Console.WriteLine("Deseja comprar algum item?  ");
+
+                resposta = Console.ReadLine();
+
+                Console.WriteLine("Compra realizada com sucesso! ");
+            }
+            while (resposta == "sim");
+
+
+            Console.WriteLine("Programa finalizado! ");
+        }
+    }
+
+        }
+    
+
